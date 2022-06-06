8ee007ee9a7fbbae0172678f598f2ac76bca62a0
Alessio Montuoro <alessio@montuoro.info>
2022-06-06 09:17:27 +0200

    added alt homerow mod

--- keyboards/splitkb/kyria/keymaps/alessio/keymap.c
+++ keyboards/splitkb/kyria/keymaps/alessio/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_BASE] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        DE_Z,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_RALT,
-     KC_LSFT , KC_A ,  KC_S   ,  LCTL_T(KC_D)  ,   LSFT_T(KC_F) ,   KC_G ,                                        KC_H,   RSFT_T(KC_J) ,  RCTL_T(KC_K) ,   KC_L ,KC_SCLN,CTL_QUOT,
+     KC_LSFT , KC_A ,  LALT_T(KC_S)   ,  LCTL_T(KC_D)  ,   LSFT_T(KC_F) ,   KC_G ,                                        KC_H,   RSFT_T(KC_J) ,  RCTL_T(KC_K) ,   RALT_T(KC_L) ,KC_SCLN,CTL_QUOT,
      MT(MOD_LCTL,KC_TAB) , DE_Y ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 _______, KC_LGUI, KC_BSPC, KC_SPC , NAV_ENTER   , SYM_ENTER  , KC_SPC ,KC_DEL, UML, KC_APP
     ),
